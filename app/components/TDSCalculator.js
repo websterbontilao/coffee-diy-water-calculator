53e3f1b08ab254383323364968d22a38ac19b311
Webster Dave Bontilao <websterbontilao@gmail.com>
2023-06-04 23:10:59 +0800

    Moved TDS calculator to a new js file

---
+++ app/components/TDSCalculator.js
@@ -0,0 +1,26 @@
+"use client";
+
+export function calculateTDS (
+    gh,
+    kh,
+    initial_tds = 0
+) {
+    let tds = initial_tds + gh / 1000 * 1200 + kh / 1000 * 1680;
+
+    return tds || 0;
+}
+
+export function calculateGH(gh, water, initial_gh = 0) {
+
+    let ret = ((initial_gh * water) + (1001.41 * gh)) / 1000;
+
+    return ret || 0;
+}
+
+export function calculateKH(kh, water, initial_kh = 0) {
+
+    let ret = ((initial_kh * water) + (1002.46 * kh)) / 1000;
+
+    return ret || 0;
+}
+
