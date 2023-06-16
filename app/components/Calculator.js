3e1a904e32b8bff22ac394ce6b9c1c1368f0fd52
websterbontilao@gmail.com <websterbontilao@gmail.com>
2023-06-16 15:11:28 +0800

    updated separation of concerns for components

--- app/components/Calculator.js
+++ app/components/Calculator.js
@@ -1,71 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
-import { calculateTDS, calculateGH, calculateKH, calculateWater } from "./TDSCalculator";
 
-import Number from "./fields/NumberField";
+import TDSCalculator from "./TDSCalculator";
 
 export default function Calculator() {
 
-    const [waterValue, setWaterValue] = useState(1000);
-    const [initialGhValue, setInitialGhValue] = useState('');
-    const [initialKhValue, setInitialKhValue] = useState('');
-    const [ghValue, setGhValue] = useState('');
-    const [khValue, setKhValue] = useState('');
-
-    let ghResult = calculateGH(ghValue, waterValue, initialGhValue);
-    let khResult = calculateKH(khValue, waterValue, initialKhValue);
-
-    let tdsValue = calculateTDS(ghResult, khResult);
-
-    function handleGHChange(e) {
-
-        setGhValue(e.target.value);
-    }
-
-    function handleInitialGhChange(e) {
-
-        setInitialGhValue(e.target.value);
-    }
-
-    function handleKHChange(e) {
-
-        setKhValue(e.target.value);
-    }
-
-    function handleInitialKhChange(e) {
-
-        setInitialKhValue(e.target.value);
-    }
-
-    function handleWaterChange(e) {
-
-        setWaterValue(e.target.value);
-    }
-
     return (
-        <div className="flex flex-col flex-nowrap ml-5 mt-5">
-            <div className="w-1/5">
-             <Number label="Total Water (mL)" value={waterValue} onChange={handleWaterChange} />
-            </div>
-            <div className="flex flex-row">
-                <Number label="Initial GH (ppm)" value={initialGhValue} onChange={handleInitialGhChange} />
-                <span className="ml-1">
-                    <Number label="GH (mL)" value={ghValue} onChange={handleGHChange} />
-                </span>
-            </div>
-            <div className="flex flex-row">
-                <Number label="Initial KH (ppm)" value={initialKhValue} onChange={handleInitialKhChange} />
-                <div className="ml-1 flex-none">
-                    <Number label="KH (mL)" value={khValue} onChange={handleKHChange} />
-                </div> 
-            </div>
-           
-            <br />
-            <div className="w-1/5">
-                <Number label="Resulting GH" value={ghResult} disabled={true} />
-                <Number label="Resulting KH" value={khResult} disabled={true} />
-                <Number label="Resulting TDS" value={tdsValue} disabled={true} />
-            </div>
-        </div>
+        <>
+            <TDSCalculator />
+        </>
     );
 }
